903da5f8c17fd809f95e513ca27a8b606a68e8d8
skyler-oakeson <skyler.oakeson@gmail.com>
2025-01-20 15:20:54 -0700

    Init grado-cli.

---
+++ grado-cli/target/debug/deps/grado_cli-afaf77ea8d67da24.d
@@ -0,0 +1,5 @@
+/home/skyler/Code/CanvasTestRunner/grado-cli/target/debug/deps/grado_cli-afaf77ea8d67da24: src/main.rs
+
+/home/skyler/Code/CanvasTestRunner/grado-cli/target/debug/deps/grado_cli-afaf77ea8d67da24.d: src/main.rs
+
+src/main.rs:
---
+++ grado-cli/target/debug/grado-cli.d
@@ -0,0 +1,1 @@
+/home/skyler/Code/CanvasTestRunner/grado-cli/target/debug/grado-cli: /home/skyler/Code/CanvasTestRunner/grado-cli/src/main.rs
